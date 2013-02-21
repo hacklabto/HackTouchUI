c7effc9cf5464952eaa89c139afa54d0965e2bf8
Martin Ringehahn <martin@ringehahn.de>
2013-02-21 00:20:57 -0500

    add vagrant development environment

--- README.md
+++ README.md
@@ -20,3 +20,9 @@ This will start the server with a self-certified ssl certificate. If you need a 
 
 1. `unicorn -p 8081`
 
+Development
+-----------
+
+You can use [vagrant](http://www.vagrantup.com/) to fire up a development VM with all the neccessary dependencies within. You will get a ready to launch environment (rvm, ruby, bundled gems will be deployed via puppet).
+
+Just run `vagrant up`. Once the VM is running, you can ssh into it via `vagrant ssh`. The code can be found in `/vagrant`, which is shared between your host and the VM. Port 3000 on localhost gets redirected to port 3000 on the development vm, so make sure you're running unicorn (or thin) on the right port.
---
+++ puppet/manifests/default.pp
@@ -0,0 +1,88 @@
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rvm"
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+# --- SQLite -------------------------------------------------------------------
+
+package { ['sqlite3', 'libsqlite3-dev']:
+  ensure => installed;
+}
+
+# --- Packages -----------------------------------------------------------------
+
+package { 'curl':
+  ensure => installed
+}
+
+package { 'build-essential':
+  ensure => installed
+}
+
+package { 'git-core':
+  ensure => installed
+}
+
+# Nokogiri dependencies.
+package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
+  ensure => installed
+}
+
+# ExecJS runtime.
+package { 'nodejs':
+  ensure => installed
+}
+
+# hacktouch
+package { 'mpd':
+  ensure => installed
+}
+
+# --- Ruby ---------------------------------------------------------------------
+
+exec { 'install_rvm':
+  command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+  creates => "${home}/.rvm",
+  require => Package['curl']
+}
+
+exec { 'install_ruby':
+  # We run the rvm executable directly because the shell function assumes an
+  # interactive environment, in particular to display messages or ask questions.
+  # The rvm executable is more suitable for automated installs.
+  #
+  # Thanks to @mpapis for this tip.
+  command => "${as_vagrant} '${home}/.rvm/bin/rvm install 1.9.3 && rvm alias create default 1.9.3'",
+  creates => "${home}/.rvm/bin/ruby",
+  require => Exec['install_rvm']
+}
+
+exec { "install_bundler":
+  command => "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'",
+  creates => "${home}/.rvm/bin/bundle",
+  require => Exec['install_ruby']
+}
+
+exec { 'bundle_install':
+  command => "${as_vagrant} 'bundle install'",
+  unless => "${as_vagrant} 'bundle check'",
+  cwd => '/vagrant',
+  require => Exec['install_bundler'],
+}
